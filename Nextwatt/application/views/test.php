41b8c26315033f3b30833547e12ed817c9621236
ElAndy13 <shany.guitou@viacesi.fr>
2014-05-22 17:17:41 +0200

    Revert
    Revert to "Test 5"

--- Nextwatt/application/views/test.php
+++ Nextwatt/application/views/test.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,31 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap 101 Template</title>
-=======
+
 <link href="<?php echo $bootstrapcss ?>" rel="stylesheet">
->>>>>>> f156d7b0c5473285ac2135b9790f3e8fccdddb15
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-
-<<<<<<< HEAD
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
-=======
 <div class="navbar navbar-default">
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
@@ -81,4 +64,3 @@
 <script language="JavaScript" type="text/javascript" src="<?php echo $bootstrapmin ?>"></script> 
 <script language="JavaScript" type="text/javascript" src="<?php echo $bootstrap ?>"></script>
 
->>>>>>> f156d7b0c5473285ac2135b9790f3e8fccdddb15
