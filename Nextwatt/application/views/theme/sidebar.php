92f273288e77c55cdbcfcad0c1f5e07669439cc9
Koko <kevin.nerino@viacesi.fr>
2014-05-23 15:27:02 +0200

    Cococommit !

--- Nextwatt/application/views/theme/sidebar.php
+++ Nextwatt/application/views/theme/sidebar.php
@@ -8,10 +8,12 @@
         <title>Bootstrap 101 Template</title>
         <link href="<?php echo $simplesidebar ?>" rel="stylesheet">
     <body>
+        <div class="page">
+            <h1>Test</h1>
+        </div>
+<!--        <div id="wrapper">
 
-        <div id="wrapper">
-
-            <!-- Sidebar -->
+             Sidebar 
             <div id="sidebar-wrapper">
                 <ul class="sidebar-nav">
                     <li class="sidebar-brand"><img src="<?php echo $minilogonextwatt ?>" alt="Mini Logo Nextwatt" />
@@ -33,7 +35,18 @@
                 </ul>
             </div>
 
-            <!-- Page content -->
+             Page content 
+        </div>-->
+        <div class="sidebar">
+            <div class="sidebar-profil">
+                <img src="<?php echo $minilogonextwatt ?>"/>
+                <br/>
+                <strong>Nextwatt</strong>
+            </div>
+            <a class="sidebar-item">Menu 1</a>
+            <a class="sidebar-item">Menu 2</a>
+            <a class="sidebar-item">Menu 3</a>
+            <a class="sidebar-item">Menu 4</a>
         </div>
     </body>
     <script language="JavaScript" type="text/javascript" src="<?php echo $jquery ?>"></script> 
