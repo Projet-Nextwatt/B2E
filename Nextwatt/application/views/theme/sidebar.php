89f46099f82927bd16f14043e414a0ea73f8bb0b
Koko <kevin.nerino@viacesi.fr>
2014-05-23 17:29:03 +0200

    Commit avant week end
    Bug de z-index

--- Nextwatt/application/views/theme/sidebar.php
+++ Nextwatt/application/views/theme/sidebar.php
@@ -1,43 +1,17 @@
-
 <!DOCTYPE html>
-<html lang="fr">
+<html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap 101 Template</title>
-        <link href="<?php echo $simplesidebar ?>" rel="stylesheet">
+        <title>Test sidebar</title>
+        <link  rel="stylesheet" href="<?php echo $simplesidebar ?>">
+    </head>
     <body>
-        <div class="page">
-            <h1>Test</h1>
+        <div class="contenu" id="contenu">
+            <span>Je suis un test</span>
         </div>
-<!--        <div id="wrapper">
-
-             Sidebar 
-            <div id="sidebar-wrapper">
-                <ul class="sidebar-nav">
-                    <li class="sidebar-brand"><img src="<?php echo $minilogonextwatt ?>" alt="Mini Logo Nextwatt" />
-                    </li>
-                    <li><a href="#">Dashboard</a>
-                    </li>
-                    <li><a href="#">Shortcuts</a>
-                    </li>
-                    <li><a href="#">Overview</a>
-                    </li>
-                    <li><a href="#">Events</a>
-                    </li>
-                    <li><a href="#">About</a>
-                    </li>
-                    <li><a href="#">Services</a>
-                    </li>
-                    <li><a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-
-             Page content 
-        </div>-->
-        <div class="sidebar">
+        <div class="sidebar" >
             <div class="sidebar-profil">
                 <img src="<?php echo $minilogonextwatt ?>"/>
                 <br/>
@@ -49,7 +23,4 @@
             <a class="sidebar-item">Menu 4</a>
         </div>
     </body>
-    <script language="JavaScript" type="text/javascript" src="<?php echo $jquery ?>"></script> 
-    <script language="JavaScript" type="text/javascript" src="<?php echo $bootstrap ?>"></script>
-
 </html>
