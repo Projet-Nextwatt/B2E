b9e154836edbec2728e71cb64934adf5f82859e6
Koko <kevin.nerino@viacesi.fr>
2014-05-24 23:51:18 +0200

    Mise au propre 2

--- Nextwatt/application/controllers/test.php
+++ Nextwatt/application/controllers/test.php
@@ -3,19 +3,25 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Test extends CI_Controller {
+class Test extends CI_Controller
+{
 
-    public function index() {
+    public function index()
+    {
         $this->load->helper('url');
         $this->load->helper('assets_helper');
 
         $data = array();
-        $data['jquery'] = js_url('jquery.min');
-        $data['bootstrap'] = js_url('bootstrap');
-        $data['bootstrapmin'] = js_url('bootstrap.min');
-        $data['test'] = js_url('test');
-        $data['bootstrapcss'] = css_url('bootstrap');
-        $this->load->view('test', $data);
+//        $data['jquery'] = js_url('jquery.min');
+        $data['jquery'] = 'http://localhost/B2E/Nextwatt/application/helpers/assets/javascript/jquery.min.js';
+        $data['zeptomin'] = js_url('zepto.min');
+        $data['hammermin'] = js_url('hammer.min');
+        $data['zeptofx'] = js_url('zepto.fx');
+        $data['sidebar'] = js_url('sidebar');
+        $data['simplesidebar'] = css_url('simple-sidebar');
+        $data['minilogonextwatt'] = img_url('logonextwatt.gif');
+        //$this->load->view('test', $data);
+        $this->load->view('theme/sidebar', $data);
     }
 
 }
