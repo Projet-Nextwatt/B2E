595fa3ce222db3cb35703300693382630b54f025
Koko <kevin.nerino@viacesi.fr>
2014-05-26 14:37:03 +0200

    Commit avec la sidebar à jour avec le bon hammer

--- Nextwatt/application/helpers/assets/javascript/sidebar.js
+++ Nextwatt/application/helpers/assets/javascript/sidebar.js
@@ -1,51 +1,50 @@
-(function ($) {
-    var $contenu = $('#contenu');
+$(function ($) {
+    var contenu = document.getElementById("contenu");
     var sidebar = 0;
-    $contenu.hammer()
-        .on('swiperight', function (e) {
-            if (sidebar == 0) {
-                return true;
-            }
-            $contenu.animate({translateX: "300px"}, 200);
-            sidebar = 1;
-        })
-        .on('swipeleft', function (e) {
-            if (sidebar == 1) {
-                return true;
-            }
-            $contenu.animate({translateX: "0px"}, 200);
-            sidebar = 0;
-        })
-        .on('drag', function (e) {
-            if (e.gesture.deltaX > 300) {
-                return false;
-            }
-            if (e.gesture.direction == 'right' && !sidebar) {
-                $contenu.animate({translateX: e.gesture.deltaX + "px"}, 0);
+    Hammer(contenu).on("swiperight", function () {
+        if (sidebar) {
+            return true;
+        }
+        $(this).animate({translateX: "300px"}, 200);
+        sidebar = 1;
+    });
+    Hammer(contenu).on("swipeleft", function () {
+        if (!sidebar) {
+            return true;
+        }
+        $(this).animate({translateX: "0px"}, 200);
+        sidebar = 0;
+    });
+    Hammer(contenu).on('drag', function (e) {
+        if (e.gesture.deltaX > 300) {
+            return false;
+        }
+        if (e.gesture.direction == 'right' && !sidebar) {
+            $(this).animate({translateX: e.gesture.deltaX + "px"}, 0);
+
+        }
+        if (e.gesture.direction == 'left' && sidebar) {
+            $(this).animate({translateX: (300 + e.gesture.deltaX) + "px"}, 0);
+        }
+    });
+    Hammer(contenu).on('dragend', function (e) {
+        if (e.gesture.direction == 'right' && !sidebar) {
+            if (e.gesture.deltaX > 150) {
+                $(this).animate({translateX: "300px"}, 200);
+                sidebar = 1;
+            } else {
+                $(this).animate({translateX: "0px"}, 200);
 
             }
-            if (e.gesture.direction == 'left' && sidebar) {
-                $contenu.animate({translateX: (300 + e.gesture.deltaX) + "px"}, 0);
-            }
-        })
-        .on('dragend', function (e) {
-            if (e.gesture.direction == 'right' && !sidebar) {
-                if (e.gesture.deltaX > 150) {
-                    $contenu.animate({translateX: "300px"}, 200);
-                    sidebar = 1;
-                } else {
-                    $contenu.animate({translateX: "0px"}, 200);
+        }
+        if (e.gesture.direction == 'left' && sidebar) {
+            if (e.gesture.deltaX < -150) {
+                $(this).animate({translateX: "0px"}, 200);
+                sidebar = 0;
+            } else {
+                $(this).animate({translateX: "300px"}, 200);
 
-                }
             }
-            if (e.gesture.direction == 'left' && sidebar) {
-                if (e.gesture.deltaX < -150) {
-                    $contenu.animate({translateX: "0px"}, 200);
-                    sidebar = 0;
-                } else {
-                    $contenu.animate({translateX: "300px"}, 200);
-
-                }
-            }
-        })
-})(Zepto);
+        }
+    });
+})
