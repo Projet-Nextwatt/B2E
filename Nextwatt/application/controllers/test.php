c91a9fe75d348e6f78db244860a484a4d6ce56c4
Koko <kevin.nerino@viacesi.fr>
2014-05-26 17:27:53 +0200

    Commit avec la sidebar (volé) qui marche bien comme il faut ! Lol

--- Nextwatt/application/controllers/test.php
+++ Nextwatt/application/controllers/test.php
@@ -12,12 +12,7 @@ class Test extends CI_Controller
         $this->load->helper('assets_helper');
 
         $data = array();
-//        $data['jquery'] = js_url('jquery.min');
-        $data['jquery'] = 'http://localhost/B2E/Nextwatt/application/helpers/assets/javascript/jquery.min.js';
-        $data['zeptomin'] = js_url('zepto.min');
-        $data['hammermin'] = js_url('hammer.min');
-        $data['zeptofx'] = js_url('zepto.fx');
-        $data['sidebar'] = js_url('sidebar');
+
         $data['simplesidebar'] = css_url('simple-sidebar');
         $data['minilogonextwatt'] = img_url('logonextwatt.gif');
         //$this->load->view('test', $data);
