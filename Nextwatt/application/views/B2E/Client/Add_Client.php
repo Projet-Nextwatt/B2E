8964b1ca21a5f712575828132c504392b0d219eb
Koko <kevin.nerino@viacesi.fr>
2014-06-06 12:08:13 +0200

    Formulaire arrangé

--- Nextwatt/application/views/B2E/Client/Add_Client.php
+++ Nextwatt/application/views/B2E/Client/Add_Client.php
@@ -66,6 +66,7 @@
 
                 </div>
 
+
                 <ul class="nav nav-list">
                     <!-- first level item -->
                     <li>
@@ -128,99 +129,101 @@
                             <small><i class="ace-icon fa fa-angle-double-right"></i> Accueil des clients</small>
                         </h1>
                     </div>
+<div class="row">
+    <div class="col-xs-12">
+        <form class="form-horizontal" role="form">
 
-                    <form class="form-horizontal" role="form">
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-5 control-label">Civilité</label>
-                            <div class="col-sm-3">
-                                <select name="myselect" id="civilite" class="dropdown">
-                                    <option value="1" >Madame</option>
-                                    <option value="2">Mademoiselle</option>
-                                    <option value="3" selected>Monsieur</option>
-                                </select>
-
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-5 control-label">Nom et Prénom </label>
-                            <div class="col-sm-2">
-                                <input type="text" class="form-control" id="nom1" placeholder="Votre Nom">
-                            </div>
-                            <div  class="col-sm-2">
-                                <input  type="text" class="form-control" id="prenom1" placeholder="Votre prénom">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-5 control-label">Nom et Prénom (Conjoint)</label>
-                            <div class="col-sm-2">
-                                <input type="text" class="form-control"id="nom2" placeholder="Votre Nom">
-                            </div>
-                            <div  class="col-sm-2">
-                                <input  type="text" class="form-control" id="prenom2" placeholder="Votre prénom">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-5 control-label">Adresse</label>
-                            <div class="col-sm-4">
-                                <textarea class="col-sm-9" rows="3" id="adresse" placeholder="Votre adresse"></textarea>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-5 control-label">Code Postal et Vile </label>
-                            <div class="col-sm-2">
-                                <input type="text" class="form-control" id="codepostal" placeholder="Code Postal">
-                            </div>
-                            <div  class="col-sm-2">
-                                <input  type="text" class="form-control" placeholder="Ville">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-5 control-label">Email</label>
-                            <div class="col-sm-3">
-                                <input type="email" class="form-control" id="email" placeholder="Email" required value>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputPassword3" class="col-sm-5 control-label">Téléphone (fixe et/ou portable)</label>
-                            <div class="col-sm-2">
-                                <input type="text" class="form-control" id="tel1" placeholder="Teléphone fixe">
-                            </div>
-                            <div class="col-sm-2">
-                                <input type="text" class="form-control" id="tel2" placeholder="Teléphone portable">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-5 control-label">Responsable</label>
-                            <div class="col-sm-3">
-                                <select name="myselect" class="dropdown">
-                                    <option value="1">Stephane</option>
-                                    <option value="2">Xavier</option>
-                                    <option value="2">Marc</option>
-                                </select>
-                            </div>
-                        </div></br>
-
-                        <div class="form-group">
-                            <div class="col-sm-offset-5 col-sm-2">
-                                <a href="<?php echo site_url("client/verif_form_client"); ?>"<button class="btn btn-sm btn-info btn-white">
-                                    <i class="ace-icon fa fa-floppy-o bigger-160"></i>
-                                    Enregistrer
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
-
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label">Civilité</label>
+                <div class="col-sm-3">
+                    <select name="myselect" id="civilite" class="dropdown">
+                        <option value="1" >Madame</option>
+                        <option value="2">Mademoiselle</option>
+                        <option value="3" selected>Monsieur</option>
+                    </select>
 
                 </div>
+            </div>
+
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label">Nom et Prénom </label>
+                <div class="col-sm-2">
+                    <input type="text" class="form-control" id="nom1" placeholder="Votre Nom">
+                </div>
+                <div  class="col-sm-2">
+                    <input  type="text" class="form-control" id="prenom1" placeholder="Votre prénom">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label">Nom et Prénom (Conjoint)</label>
+                <div class="col-sm-2">
+                    <input type="text" class="form-control"id="nom2" placeholder="Votre Nom">
+                </div>
+                <div  class="col-sm-2">
+                    <input  type="text" class="form-control" id="prenom2" placeholder="Votre prénom">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label">Adresse</label>
+                <div class="col-sm-4">
+                    <textarea class="col-sm-9" rows="3" id="adresse" placeholder="Votre adresse"></textarea>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label">Code Postal et Vile </label>
+                <div class="col-sm-2">
+                    <input type="text" class="form-control" id="codepostal" placeholder="Code Postal">
+                </div>
+                <div  class="col-sm-2">
+                    <input  type="text" class="form-control" placeholder="Ville">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label">Email</label>
+                <div class="col-sm-3">
+                    <input type="email" class="form-control" id="email" placeholder="Email" required value>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="inputPassword3" class="col-sm-5 control-label">Téléphone (fixe et/ou portable)</label>
+                <div class="col-sm-2">
+                    <input type="text" class="form-control" id="tel1" placeholder="Teléphone fixe">
+                </div>
+                <div class="col-sm-2">
+                    <input type="text" class="form-control" id="tel2" placeholder="Teléphone portable">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label">Responsable</label>
+                <div class="col-sm-3">
+                    <select name="myselect" class="dropdown">
+                        <option value="1">Stephane</option>
+                        <option value="2">Xavier</option>
+                        <option value="2">Marc</option>
+                    </select>
+                </div>
+            </div></br>
+
+            <div class="form-group">
+                <div class="col-sm-offset-5 col-sm-2">
+                    <a href="<?php echo site_url("client/verif_form_client"); ?>"<button class="btn btn-sm btn-info btn-white">
+                        <i class="ace-icon fa fa-floppy-o bigger-160"></i>
+                        Enregistrer
+                    </button></a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+                </div>
+
                 <!-- /.page-content -->
             </div>
             <!-- /.main-content -->
@@ -246,11 +249,5 @@
         <script src="<?php echo $acemin ?>"></script>
         <script src="<?php echo $aceelementsmin ?>"></script>
 
-        <script type="text/javascript">
-            //If page has any inline scripts, it goes here
-            //    jQuery(function ($) {
-            //        alert('hello!');
-            //    });
-        </script>
     </body>
 </html>
