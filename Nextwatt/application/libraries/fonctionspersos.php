1e29e71754ae59c17c14cf12b7f0553adaa5f637
LyannNextwatt <y.longere@nextwatt.fr>
2014-06-17 11:46:14 +0200

    Addition du Ace à la fonction creerTableau

--- Nextwatt/application/libraries/fonctionspersos.php
+++ Nextwatt/application/libraries/fonctionspersos.php
@@ -1,21 +1,24 @@
 <?php
-class Fonctionspersos
-{
-    public function creerTableau(array $entetes, array $contenu)
-    {
-        if ($contenu == NULL OR (isset($contenu[0]) AND $contenu[0] == '')) {
+
+class Fonctionspersos {
+
+    public function creerTableau(array $entetes, array $contenu) {
+        if ($contenu == NULL OR ( isset($contenu[0]) AND $contenu[0] == '')) {
             echo '<p>Attention: Aucune donn&eacutees &agrave; afficher dans le tableau.</p>';
         } else {
-            echo '<table>';
+            echo '<table id="sample-table-1" class="table table-striped table-bordered table-hover">';
 
             //En-tete
+            echo '<thead>'."\n";
             echo '<tr>' . "\n";
             foreach ($entetes as $entete) {
                 echo '<th>' . $entete . '</th>' . "\n";
             }
             echo '</tr>' . "\n";
+            echo '</thead>'."\n";
 
             //Contenu
+            echo '<tbody>'."\n";
             foreach ($contenu as $ligne) {
                 echo '<tr>' . "\n";
                 foreach ($ligne as $cellule) {
@@ -23,10 +26,9 @@ class Fonctionspersos
                 }
                 echo '</tr>' . "\n";
             }
-
+            echo '</tbody>'."\n";
             echo '</table>';
+            
         }
     }
 }
-
-
