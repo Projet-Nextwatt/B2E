efa9554cc4bd6d8bbfd66ce9f662c9d8751106e1
LyannNextwatt <y.longere@nextwatt.fr>
2014-06-17 11:02:30 +0200

    Récup de la librairie fonctionsperso

--- Nextwatt/application/views/B2E/Parametre/choix_parametre.php
+++ Nextwatt/application/views/B2E/Parametre/choix_parametre.php
@@ -1,14 +1,14 @@
 <div class="ace-settings-container" id="ace-settings-container">
     <!-- settings box goes here -->
-    <p>Ace-setting...</p>
 </div>
 
 <div class="page-header">
-    <p>page-header</p>
 </div>
 
 <div class="row">
-    <a class="btn btn-primary" href="<?php echo site_url("parametre/Consulter_Energie"); ?>">
-        <i class="ace-icon fa fa-flash align-top bigger-125"/></i>Prix de l'&eacute;nergie
-    </a>
+    <div class="col-xs-12">
+        <a class="btn btn-primary" href="<?php echo site_url("parametre/Consulter_Energie"); ?>">
+            <i class="ace-icon fa fa-flash align-top bigger-125"/></i>Prix de l'&eacute;nergie
+        </a>
+    </div>
 </div>
