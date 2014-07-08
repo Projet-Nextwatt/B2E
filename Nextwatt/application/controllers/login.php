5e453dd9d542caa0e499b419051d91142c7c45ad
ElAndy13 <shany.guitou@viacesi.fr>
2014-07-08 16:49:52 +0200

    Mise en place Login
    Mise en place login côté php + recup taff kévin en AJAX

--- Nextwatt/application/controllers/login.php
+++ Nextwatt/application/controllers/login.php
@@ -8,31 +8,17 @@ class Login extends CI_Controller
 
     public function index()
     {
-        $this->load->helper('url');
-        $this->load->helper('assets_helper');
 
-        $data = array();
-
-        // Liens vers les fichiers javascripts
-        $data['jquerymin'] = js_url('jquery.min');
-        $data['bootstrapmin'] = js_url('bootstrap.min');
-        $data['acemin'] = js_url('ace.min');
-        $data['aceelementsmin'] = js_url('ace-elements.min');
-
-
-        // Liens vers les fichiers CSS
-        $data['bootstrapmincss'] = css_url('bootstrap.min');
-        $data['acefonts'] = css_url('ace-fonts');
-        $data['acemincss'] = css_url('ace.min');
-        $data['acertl'] = css_url('ace-rtl.min');
-        $data['aceskins'] = css_url('ace-skins.min');
-
-        // Liens vers les fichiers images
-        $data['minilogonextwatt'] = img_url('MiniLogoNextwatt.png');
-        $data['blur'] = img_url('blurNextwatt.png');
-
-        // Charge la page
-        $this->load->view('B2E/Login', $data);
     }
 
+    public function ajax_login()
+    {
+        if(isset($_POST['login']) && isset($_POST['password']))
+        {
+            $this->load->model('Mappage/user', 'mapuser');
+            $retourmodel = $this->mapuser->verif_login();
+
+            echo($retourmodel);
+        }
+    }
 }
