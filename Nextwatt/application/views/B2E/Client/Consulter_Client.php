6e979f1a6917f7a5a26079a3883eecf52d99bb85
ElAndy13 <shany.guitou@viacesi.fr>
2014-07-18 14:46:19 +0200

    Merge remote-tracking branch 'origin/Developpement' into Developpement
    Conflicts:
    	Nextwatt/application/third_party/dompdf/lib/fonts/log.htm

--- Nextwatt/application/views/B2E/Client/Consulter_Client.php
+++ Nextwatt/application/views/B2E/Client/Consulter_Client.php
@@ -42,7 +42,7 @@
                         </li>   
                     </ul>
 
-                    
+
                     <div class="tab-content">
                         <div class="tab-pane in active" id="actif">
 
@@ -112,34 +112,52 @@
 
 
                         <div class="tab-pane" id="archives">
-                            <?php var_dump($mesclientsarchive)?>
+                            
                             <div class="panel panel-success">
                                 <!-- Default panel contents -->
                                 <div class="panel-heading align-left">Mes clients archivés</div>
                                 <?php $this->fonctionspersos->creerTableau($mesclientsarchive, $enteteclients = NULL, 'CI_client/modif_client', 'CI_client/ajax_supprimerclient '); ?>
                             </div>
 
-                            
-                            <?php if ($this->session->userdata("userconnect")["Droit_Admin"]==1 AND isset($clientsarchive)) {?> 
-                            <div id="accordion" class="accordion-style1 panel-group accordion-style2">
+                            <?php if ($this->session->userdata("userconnect")["Droit_Admin"]==1 AND isset($clients)) {?> 
+                            <div id="accordion2" class="accordion-style1 panel-group accordion-style2">
+                                <!-- 1er accordeon toujours ouvert -->
+                                <!--
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                                                <i class="bigger-110 ace-icon fa fa-angle-down" data-icon-hide="ace-icon fa fa-angle-down" data-icon-show="ace-icon fa fa-angle-right"></i>
+                                                &nbsp;Group Item #1
+                                            </a>
+                                        </h4>
+                                    </div>
+
+                                    <div class="panel-collapse collapse in" id="collapseOne" style="height: auto;">
+                                        <div class="panel-body">
+                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                                        </div>
+                                    </div>
+                                </div>
+                                -->
                                 
-                            <?php foreach ($clientsarchive as $user_id => $clientsduuser){ ?>
+                            <?php foreach ($clientsarchive as $user_id => $clientsarchiveduuser){ ?>
                                 
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
-                                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $user_id; ?>">
+                                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapsearchive<?php echo $user_id; ?>">
                                                 <i class="bigger-110 ace-icon fa fa-angle-right" data-icon-hide="ace-icon fa fa-angle-down" data-icon-show="ace-icon fa fa-angle-right"></i>
                                                 
-                                                <span class="badge"><?php echo count($clientsduuser); ?></span>
+                                                <span class="badge"><?php echo count($clientsarchiveduuser); ?></span>
                                                 <?php echo $users[$user_id]['nom'];?> 
                                             </a>
                                         </h4>
                                     </div>
 
-                                    <div class="panel-collapse collapse" id="collapse<?php echo $user_id; ?>" style="height: 0px;">
+                                    <div class="panel-collapse collapse" id="collapsearchive<?php echo $user_id; ?>" style="height: 0px;">
                                         <div class="panel-body">    
-                                            <?php $this->fonctionspersos->creerTableau($clientsduuser, $enteteclients = NULL, 'CI_client/modif_client', 'CI_client/ajax_supprimerclient '); ?>
+                                            <?php $this->fonctionspersos->creerTableau($clientsarchiveduuser, $enteteclients = NULL, 'CI_client/modif_client', 'CI_client/ajax_supprimerclient '); ?>
                                          </div>
                                     </div>
                                 </div>
@@ -150,6 +168,7 @@
 
                             </div> <!-- fin de l'accordeon -->
                             <?php } //fin du if admin?>
+                            
                         </div>
                         
                         
