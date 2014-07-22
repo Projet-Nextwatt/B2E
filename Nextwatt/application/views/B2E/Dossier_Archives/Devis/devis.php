7b27facce4f8b7139f1f56f7003f9cf2dda02cf3
Koko <kevin.nerino@viacesi.fr>
2014-07-22 16:52:21 +0200

    Nouvelle version graphique de la page de devis

--- Nextwatt/application/views/B2E/Dossier_Archives/Devis/devis.php
+++ Nextwatt/application/views/B2E/Dossier_Archives/Devis/devis.php
@@ -26,11 +26,9 @@
         <div class="col-xs-12">
             <div class="row">
                 <div class="col-md-3 col-xs-offset-2">
-                    <div class="widget-box">
-                        <div class="widget-header widget-header-small">
-                            <h4 class="widget-title">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
                                 Client
-                            </h4>
                         </div>
                         <div class="widget-body">
                             <?php
@@ -50,15 +48,13 @@
                     </div>
                 </div>
                 <div class="col-md-3 col-xs-offset-2">
-                    <div class="widget-box">
-                        <div class="widget-header widget-header-small">
-                            <h4 class="widget-title">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
                                 Tarif
-                            </h4>
                         </div>
-                        <div class="widget-body">
-                            Total TTC : <strong><?php echo $tariftotal; ?> €</strong><br/>
-                            Dont TVA : <strong><?php echo $tva; ?> €</strong>
+                        <div class="panel-body bigger-120">
+                            <p><strong>Total TTC : <?php echo $tariftotal; ?> €</strong></p>
+                            <p>Dont TVA : <?php echo $tva; ?> €</p>
                         </div>
                     </div>
                 </div>
@@ -67,76 +63,71 @@
 
             <div class="row">
                 <div class="col-xs-8 col-xs-offset-2">
-                    <div class="widget-box">
-                        <div class="widget-header">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
                             Votre devis
-                            <div class="widget-toolbar no-border">
+                            <div class="" style="float: right">
                                 <a href="<?php echo site_url("CI_catalogue/consult_catalogue_devis"); ?>">
-                                    <button class="btn btn-minier btn-primary">
-                                        <i class="ace-icon fa fa-plus-square-o bigger-250"></i>
+                                    <button class="btn btn-white btn-inverse btn-bold btn-xs">
+                                        <i class="ace-icon fa fa-plus-square bigger-110"></i>
+                                        Ajouter un article
                                     </button>
+<!--                                    <button class="btn btn-info btn-sm">-->
+<!--                                        <i class="ace-icon fa fa-plus-square  bigger-110 icon-only"></i>-->
+<!--                                        <i class="ace-icon fa fa-plus-square-o bigger-200"></i>-->
+<!--                                    </button>-->
                                 </a>
                             </div>
                         </div>
-                        <div class="widget-body">
+                        <div class="panel-body">
                             <div>
-                                <table>
-                                    <thead>
-                                    <tr>
-                                        <td>Libellé</td>
-                                        <td>Prix TTC</td>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <?php foreach ($article->result() as $a) { ?>
-                                        <tr>
-                                            <td>
-                                                <?php echo $a->Marque; ?>
-                                            </td>
+                                    <div class="widget-box transparent" style="opacity: 1; z-index: 0; margin: 5px">
+                                        <div class="widget-header">
+                                            <h4 class="widget-title lighter green" >Kit 9K Aerovoltaique</h4>
 
-                                            <td>
-                                                <?php echo $a->Prix_Annonce_TTC; ?>
-                                            </td>
-                                        </tr>
+                                        </div>
 
-                                        <tr>
-                                            <td>
-                                                Main d'oeuvre
-                                            </td>
+                                        <div class="widget-body">
+                                            <div class="widget-main padding-6 no-padding-left no-padding-right" style="margin: 5px;">
+                                                <div>36 panneaux Systovi 250 Wc - Rvolt <p class="bigger-110" style="float: right">11 386,75 €</p><br/>
+                                                Onduleur Kostal Piko 8,3 <br/>
+                                                Intégration Systovi<br/>
+                                                Norme EN61215 ou NF EN61646 <br/>
+                                                Module de ventilation pour récupération de chaleur et rafraichissement</div><br/><br/>
+                                                <div class="bigger-110">Main d'oeuvre : <p style="float: right">4 000,00 €</p></div><br/>
+                                                <div class="bigger-110">Garantie materiel : 20 ans</div><br/>
+                                                <div class="hr hr-4 hr-double"></div>
+                                                <div class="bigger-110 ">Total TTC : <p style="float: right;">15 386,75 €</p></div><br/>
+                                                <div class="bigger-110">Dont TVA : <p style="float: right">1 234,00 €</p></div><br/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-box transparent" style="opacity: 1; z-index: 0; margin: 5px">
+                                        <div class="widget-header">
+                                            <h4 class="widget-title lighter green" >Pompe à chaleur Air / Air Multisplit</h4>
 
-                                            <td>
-                                                <?php echo $a->Prix_MO; ?>
-                                            </td>
-                                        </tr>
+                                        </div>
 
-                                        <tr>
-                                            <td>
-                                                Garantie materiel : <?php echo $a->Libelle_Garantie; ?>
-                                            </td>
-
-                                            <td>
-
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                Total TTC
-                                            </td>
-
-                                            <td>
-                                                <?php echo $a->Prix_Annonce_TTC + $a->Prix_MO; ?>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                Dont TVA
-                                            </td>
-
-                                            <td>
-                                                <?php echo ($a->Prix_Annonce_TTC + $a->Prix_MO) / (1 + 0.2) * 0.2; ?>
-                                            </td>
-                                        </tr>
-                                    <?php } ?>
+                                        <div class="widget-body">
+                                            <div class="widget-main padding-6 no-padding-left no-padding-right" style="margin: 5px;">
+                                                <div> Daikin MultiSplit
+                                                    2MXS40H  <p class="bigger-110" style="float: right">11 386,75 €</p><br/>
+                                                    Une pompe à Chaleur Air/AIR Réversible de type : Daikin MultiSplit
+                                                    2MXS40H<br/>COP 4,16 Certifié Eurovent + pose, accessoires et mise
+                                                    en service inclus
+                                                </div><br/><br/>
+                                                <div class="bigger-110">Main d'oeuvre : <p style="float: right">4 000,00 €</p></div><br/>
+                                                <div class="bigger-110">Garantie materiel : 20 ans</div><br/>
+                                                <div class="hr hr-2 hr-dotted"></div>
+                                                <div> Unité intérieur Daikin Console 2x voie FVXS50F  <p class="bigger-110" style="float: right">11 386,75 €</p><br/>
+                                                </div><br/><br/>
+                                                <div class="bigger-110">Main d'oeuvre : <p style="float: right">4 000,00 €</p></div><br/>
+                                                <div class="hr hr-4 hr-double"></div>
+                                                <div class="bigger-110 ">Total TTC : <p style="float: right;">15 386,75 €</p></div><br/>
+                                                <div class="bigger-110">Dont TVA : <p style="float: right">1 234,00 €</p></div><br/>
+                                            </div>
+                                        </div>
+                                    </div>
                                     </tbody>
                                 </table>
                             </div>
